92acaa1a793613323be54379f7f3743b550ef562
Jason YiZhang Chen <a880074@gmail.com>
2013-12-24 22:53:38 +0800

    Add keyword count & claims text file output.

--- processor.pl
+++ processor.pl
@@ -7,15 +7,16 @@ use String::Util ':all';
 use Data::Dump;
 use File::Basename;
 use File::Path qw/make_path/;
- 
+
 binmode(STDIN, ':encoding(utf8)');
 binmode(STDOUT, ':encoding(utf8)');
 binmode(STDERR, ':encoding(utf8)');
 
 my @BLACK_LIST;
 my @FILES;
+
 my $OUTPUT_FOLDER = './output';
-my $OUTPUT_CLAIMS = 'claims';
+my $OUTPUT_CLAIMS = 'claims.txt';
 my $OUTPUT_KEYWORD = 'keyword.txt';
 my $OUTPUT_GRAMS = 'grams-keyword.txt';
 
@@ -34,36 +35,36 @@ while ( my $line = <FHD> ) {
 close(FHD);
 
 for my $file (@FILES) {
-    $i++;
-    
     # Parse XML file
     my $dom = XML::LibXML->new->parse_file($file);
 
-    # Add elements(each word) to hash
+    # Add elements(each word) to hash & Save to text file
     for my $node ($dom->findnodes('/us-patent-grant/claims/claim/claim-text/text()')) {
         $str = $node->toString;
-        # $str =~ s/[_:;?,]/ /g;
-        # $str = lc $str;
-        # @f = split(' ', $str);
-        
+        $claims = $claims.$str;
+
         my $one_grams_ref = Text::Ngramize->new (typeOfNgrams => 'words', normalizeText => 1, sizeOfNgrams => 1)
             ->getListOfNgrams (text => \$str);
         my $two_grams_ref = Text::Ngramize->new (typeOfNgrams => 'words', normalizeText => 1, sizeOfNgrams => 2)
             ->getListOfNgrams (text => \$str);
-        
+
         $h_one{$_}++ for (@{ $one_grams_ref });
         $h_two{$_}++ for (@{ $two_grams_ref });
     }
 
-    #Print the hash out
-    print "##### $i #####\n";
-    
+    ++$i && print "##### $i #####\n";
+
+    # Save claims
+    print "Output claims...\n";
+    output_str($claims, dirname($file), $OUTPUT_FOLDER, $OUTPUT_CLAIMS);
+
+    # Save keywords
     print "Output keyword...\n";
-    output(\%h_one, $file, $OUTPUT_FOLDER, $OUTPUT_KEYWORD);
-    
+    output_hash(\%h_one, dirname($file), $OUTPUT_FOLDER, $OUTPUT_KEYWORD);
+
     print "Output 2-grans keyword...\n";
-    output(\%h_two, $file, $OUTPUT_FOLDER, $OUTPUT_GRAMS);
-    
+    output_hash(\%h_two, dirname($file), $OUTPUT_FOLDER, $OUTPUT_GRAMS);
+
     print "\n";
 }
 
@@ -74,22 +75,43 @@ sub match {
     ($_ ~~ @checkList) && return 1 for (@array); 0
 }
 
-sub output {
+sub output_hash {
     my %h = %{$_[0]};
+    my $output_path = $_[1];
+    my $FILE_FOLDER = $_[2];
+    my $FILE_NAME = $_[3];
+    my $count = 0;
+
+    $output_path =~ s/^.\/\w+/$FILE_FOLDER/;
+    $output_path = $output_path.'/'.$FILE_NAME;
+
+    make_path(dirname($output_path));
+
+    open(FHD, "+> $output_path") || die "$!\n";
+    print "$output_path\n";
+    print FHD "<count>\t\t<keyword>\n";
+    for my $key ( sort { $h{$b} <=> $h{$a} } keys %h) {
+        # Filter & Print
+        print (FHD "$h{$key}\t\t$key\n") && $count++ unless ($h{$key}==1 || match($key, \@BLACK_LIST));
+    }
+
+    print FHD "There are $count keyword(s).\n";
+    close(FHD);
+}
+
+sub output_str {
+    my $str = $_[0];
     my $output_path = $_[1];
     my $FILE_FOLDER = $_[2];
     my $FILE_NAME = $_[3];
 
     $output_path =~ s/^.\/\w+/$FILE_FOLDER/;
-    $output_path = dirname($output_path).'/'.$FILE_NAME;
-    
+    $output_path = $output_path.'/'.$FILE_NAME;
+
     make_path(dirname($output_path));
-    
+
     open(FHD, "+> $output_path") || die "$!\n";
     print "$output_path\n";
-    for my $key ( sort { $h{$b} <=> $h{$a} } keys %h) {
-        # Filter & Print
-        print FHD "\t$h{$key} \t\t $key\n" unless match($key, \@BLACK_LIST);
-    }
+    print FHD $str;
     close(FHD);
 }
